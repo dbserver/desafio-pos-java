d8fc9975fac19031ea3be88a32e8c7f0cf6b9687
renatochitolina <renato.chitolina@db.tec.br>
2023-11-01 18:19:29 -0300

    Ajustando indentação para padrão Eclipse.

--- src/Pedido.java
+++ src/Pedido.java
@@ -3,85 +3,85 @@ import java.util.ArrayList;
 import utils.InputReader;
 
 public class Pedido {
-  private static final ArrayList<PedidoItem> itens = new ArrayList<PedidoItem>();
+    private static final ArrayList<PedidoItem> itens = new ArrayList<PedidoItem>();
 
-  public static ArrayList<PedidoItem> getListaItens() {
-    return itens;
-  }
-
-  public static void adicionarItem() {
-    String nome = InputReader.getString("\nInforme o nome do produto: ");
-
-    Produto produto = Estoque.encontrarProduto(nome);
-
-    if (produto == null) {
-      System.out.println("\nProduto nao encontrado!");
-
-      return;
+    public static ArrayList<PedidoItem> getListaItens() {
+        return itens;
     }
 
-    int quantidade = InputReader.getInt("Informe a quantidade: ");
+    public static void adicionarItem() {
+        String nome = InputReader.getString("\nInforme o nome do produto: ");
 
-    boolean sucesso = adicionarItem(produto, quantidade);
+        Produto produto = Estoque.encontrarProduto(nome);
 
-    if (sucesso)
-      System.out.printf("\nAdicionado %d %s com sucesso!\n", quantidade, produto.getNome());
-    else
-      System.out.printf("\nNao foi possivel adicionar %d %s!\n", quantidade, produto.getNome());
-  }
+        if (produto == null) {
+            System.out.println("\nProduto nao encontrado!");
 
-  public static void imprimirPedido() {
-    ArrayList<PedidoItem> listaItens = getListaItens();
+            return;
+        }
 
-    System.out.println("\n------------------------------------------------------------------------");
-    System.out.println("|                                PEDIDO                                |");
-    System.out.println("|----------------------------------------------------------------------|");
-    System.out.println("| ID   | NOME             | PRECO UN     | QUANTIDADE    | TOTAL       |");
-    System.out.println("|----------------------------------------------------------------------|");
+        int quantidade = InputReader.getInt("Informe a quantidade: ");
 
-    if (listaItens.isEmpty()) {
-      System.out.println("| Ainda nao existem itens no pedido!                                   |");
-    } else {
-      for (PedidoItem itemPedido : listaItens) {
-        System.out.printf(
-            "| %-4d | %-16s | R$%-10.2f | %-13d | R$%-10.2f|\n",
-            itemPedido.getProduto().getId(),
-            itemPedido.getProduto().getNome(),
-            itemPedido.getProduto().getPreco(),
-            itemPedido.getQuantidade(),
-            itemPedido.getValorTotal());
-      }
+        boolean sucesso = adicionarItem(produto, quantidade);
+
+        if (sucesso)
+            System.out.printf("\nAdicionado %d %s com sucesso!\n", quantidade, produto.getNome());
+        else
+            System.out.printf("\nNao foi possivel adicionar %d %s!\n", quantidade, produto.getNome());
     }
 
-    System.out.println("|----------------------------------------------------------------------|");
-    System.out.printf("| %59s%-9.2f |\n", "R$", getValorTotal());
-    System.out.println("------------------------------------------------------------------------");
-  }
+    public static void imprimirPedido() {
+        ArrayList<PedidoItem> listaItens = getListaItens();
 
-  public static double getValorTotal() {
-    double valorTotal = 0;
+        System.out.println("\n------------------------------------------------------------------------");
+        System.out.println("|                                PEDIDO                                |");
+        System.out.println("|----------------------------------------------------------------------|");
+        System.out.println("| ID   | NOME             | PRECO UN     | QUANTIDADE    | TOTAL       |");
+        System.out.println("|----------------------------------------------------------------------|");
 
-    for (PedidoItem itemPedido : getListaItens())
-      valorTotal += itemPedido.getValorTotal();
+        if (listaItens.isEmpty()) {
+            System.out.println("| Ainda nao existem itens no pedido!                                   |");
+        } else {
+            for (PedidoItem itemPedido : listaItens) {
+                System.out.printf(
+                        "| %-4d | %-16s | R$%-10.2f | %-13d | R$%-10.2f|\n",
+                        itemPedido.getProduto().getId(),
+                        itemPedido.getProduto().getNome(),
+                        itemPedido.getProduto().getPreco(),
+                        itemPedido.getQuantidade(),
+                        itemPedido.getValorTotal());
+            }
+        }
 
-    return valorTotal;
-  }
+        System.out.println("|----------------------------------------------------------------------|");
+        System.out.printf("| %59s%-9.2f |\n", "R$", getValorTotal());
+        System.out.println("------------------------------------------------------------------------");
+    }
 
-  private static boolean adicionarItem(Produto produto, int quantidade) {
-    for (PedidoItem itemPedido : getListaItens()) {
-      if (itemPedido.getProduto().getNome().equalsIgnoreCase(produto.getNome())) {
-        itemPedido.setQuantidade(itemPedido.getQuantidade() + quantidade);
+    public static double getValorTotal() {
+        double valorTotal = 0;
 
-        Estoque.baixarEstoque(itemPedido.getProduto().getId(), quantidade);
+        for (PedidoItem itemPedido : getListaItens())
+            valorTotal += itemPedido.getValorTotal();
+
+        return valorTotal;
+    }
+
+    private static boolean adicionarItem(Produto produto, int quantidade) {
+        for (PedidoItem itemPedido : getListaItens()) {
+            if (itemPedido.getProduto().getNome().equalsIgnoreCase(produto.getNome())) {
+                itemPedido.setQuantidade(itemPedido.getQuantidade() + quantidade);
+
+                Estoque.baixarEstoque(itemPedido.getProduto().getId(), quantidade);
+
+                return true;
+            }
+        }
+
+        itens.add(new PedidoItem(produto, quantidade));
+
+        Estoque.baixarEstoque(produto.getId(), quantidade);
 
         return true;
-      }
     }
-
-    itens.add(new PedidoItem(produto, quantidade));
-
-    Estoque.baixarEstoque(produto.getId(), quantidade);
-
-    return true;
-  }
 }
